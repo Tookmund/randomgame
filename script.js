62e70907479375dd032452e774c4a99b7de7c30b
face-of-boe <jamieframe50@hotmail.co.uk>
2015-06-04 08:33:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+  game.load.image('pika','http://img1.wikia.nocookie.net/__cb20140410195944/pokemon/images/archive/f/fc/20150101093541!025Pikachu_OS_anime_5.png');
   
 }
 
