d8770e165537d8a9db13263fcf50249e7164a514
face-of-boe <jamieframe50@hotmail.co.uk>
2015-06-07 10:23:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+var character = prompt("Paste google url for character");
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-  game.load.image('character','http://img1.wikia.nocookie.net/__cb20140410195944/pokemon/images/archive/f/fc/20150101093541!025Pikachu_OS_anime_5.png');
+  game.load.image('character',charector);
   game.load.image('platform','http://t2.gstatic.com/images?q=tbn:ANd9GcSprZqkio6SxczT9OfaIQ37aazCfl9nAtRlPuylIWcxAHu5sSXV');
   game.load.image('background','http://t0.gstatic.com/images?q=tbn:ANd9GcRYz6d86rxtMUWhtvvaKzzSG-VxEQZJVRKC54zfcwfM_lkQqBRO');
 }
